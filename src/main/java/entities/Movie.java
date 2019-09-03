812ab697386cc16b798ce9bf5229006c3e446c11
Dennis <drh89@live.com>
2019-09-03 15:07:21 +0200

    renamed

--- src/main/java/entities/Movie.java
+++ src/main/java/entities/Movie.java
@@ -10,14 +10,14 @@ import javax.persistence.NamedQuery;
 
 @Entity
 @NamedQuery(name = "RenameMe.deleteAllRows", query = "DELETE from RenameMe")
-public class RenameMe implements Serializable {
+public class Movie implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    public RenameMe() {
+    public Movie() {
     }
         
     public Long getId() {
@@ -33,7 +33,7 @@ public class RenameMe implements Serializable {
     private String dummyStr1;
     private String dummyStr2;
 
-    public RenameMe(String dummyStr1, String dummyStr2) {
+    public Movie(String dummyStr1, String dummyStr2) {
         this.dummyStr1 = dummyStr1;
         this.dummyStr2 = dummyStr2;
     }
